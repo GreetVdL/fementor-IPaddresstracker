e74c4146260fed31d05161f3499e022a20488559
Greet Van der Linden <vanderlindengreet@gmail.com>
2021-08-09 20:06:22 +0200

    IP API implemented

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,45 @@
 import "../css/style.scss";
-import Square from "./classes/Square";
+import { Backend } from "./backend.js";
 
-new Square(20, 20, 200, document.querySelector(".test"));
-console.log(process.env.SECRET);
+// console.log(process.env.SECRET);
+
+// IP address info
+
+const ipAddress = document.querySelector("#ip");
+const city = document.querySelector("#city");
+const country = document.querySelector("#country");
+const postal = document.querySelector("#postal");
+const timezone = document.querySelector("#timezone");
+const isp = document.querySelector("#isp");
+
+const form = document.querySelector("form");
+const inputField = document.querySelector("input");
+
+const API = new Backend();
+API.setBaseUrl(
+  `https://geo.ipify.org/api/v1?apiKey=${process.env.SECRETIPKEY}`
+);
+
+API.get("").then((data) => {
+  console.log(data);
+  ipAddress.textContent = data.ip;
+  city.textContent = data.location.city + ", ";
+  country.textContent = data.location.country;
+  postal.textContent = data.location.postalCode;
+  timezone.textContent = data.location.timezone;
+  isp.textContent = data.isp;
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const input = inputField.value;
+  API.get(`&domain=${input}`).then((data) => {
+    console.log(data);
+    ipAddress.textContent = data.ip;
+    city.textContent = data.location.city + ", ";
+    country.textContent = data.location.country;
+    postal.textContent = data.location.postalCode;
+    timezone.textContent = data.location.timezone;
+    isp.textContent = data.isp;
+  });
+});
